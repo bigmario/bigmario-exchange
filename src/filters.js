04615cbe2d8e41f017964d0452a5cb9dd9527a4a
Mario Castro <mariocastro.pva@gmail.com>
2022-03-17 16:58:13 -0400

    misc changes

--- src/filters.js
+++ src/filters.js
@@ -5,7 +5,7 @@ const dollarFilter = function (value) {
         return '$ 0'
     }
 
-    return numeral(value).format('($ 0.000a)')
+    return numeral(value).format('($ 0,000,00.00 a)')
 }
 
 const percentFilter = function (value) {
@@ -13,7 +13,8 @@ const percentFilter = function (value) {
         return '0 %'
     }
 
-    return `${Number(value).toFixed(2)} %`
+    // return `${Number(value).toFixed(2)} %`
+    return numeral(value).format('0 %')
 }
 
 export { dollarFilter, percentFilter }
